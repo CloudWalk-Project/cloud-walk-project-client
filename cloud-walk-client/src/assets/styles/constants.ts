85ffcdcdb31954ba3f14a6306ffa5a95370d7cbe
Amaro Francisco <amaro_ribeir0@hotmail.com>
2022-09-20 22:13:23 -0300

    feat(home): added styles folder(theme, styled-comp

---
+++ cloud-walk-client/src/assets/styles/constants.ts
@@ -0,0 +1,15 @@
+export const constants = {
+  logoFontFamily: "Big Shoulders Inline Display",
+  logoFontSize: "28px",
+  logoLineHeight: "140%",
+
+  // headingFontFamily: "Fira Sans",
+  headingFontFamily: "Barlow",
+  headingFontSize: "28px",
+  headingLineHeight: "140%",
+
+  // bodyFontFamily: "Fira Sans",
+  bodyFontFamily: "Barlow",
+  bodyFontSize: "14px",
+  bodyLineHeight: "140%",
+};
