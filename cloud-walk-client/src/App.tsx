3e4f129bb78ec39641884c3e5acd2530f945e292
Amaro Francisco <amaro_ribeir0@hotmail.com>
2022-09-22 21:44:37 -0300

    feat(theme): ThemeProvider

--- cloud-walk-client/src/App.tsx
+++ cloud-walk-client/src/App.tsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./assets/styles/theme";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- cloud-walk-client/src/pages/home/style.ts
+++ cloud-walk-client/src/pages/home/style.ts
@@ -1,15 +1,11 @@
 import styled, { css } from "styled-components";
 import theme from "../../assets/styles/theme";
 
-interface GenresNavigationButtonProps {
-  active?: boolean;
-}
-
 export const home = styled.section`
-  ${({ theme }) => css`
-    background-color: ${theme.colors.background};
-    min-width: 100vw;
-    min-height: 100vh;
+  ${() => css`
+    background-color: ${theme.colors.primaryColor};
+    width: 100vw;
+    height: 100vh;
     color: ${theme.colors.textColor};
     display: flex;
     justify-content: space-between;
