1a319a84948e45a5f92b30c49d69066abefe2183
Amaro Francisco <amaro_ribeir0@hotmail.com>
2022-09-26 21:07:51 -0300

    feat(canva): added list and card

--- cloud-walk-client/src/pages/home/index.tsx
+++ cloud-walk-client/src/pages/home/index.tsx
@@ -1,16 +1,18 @@
 import * as S from "./style";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import CanvaCard from "../../components/CanvaCard";
-
+import CanvaList from "../../components/Canvalist";
+import { Canva, Category, User } from "../../types/interfaces";
+import { mockedCanva } from "../../mocks/canva";
 
 const Home = () => {
   return (
     <S.home>
-        <Header />
+      <Header />
       <S.HomeContent>
-        <CanvaCard/>
+        <CanvaList list={mockedCanva}></CanvaList>
       </S.HomeContent>
+      <Footer />
     </S.home>
   );
 };
