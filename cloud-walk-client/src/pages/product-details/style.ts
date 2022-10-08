e97c8dbf391a7ad92887ed593c75c74533621583
Paulo Gabriel de Angelis Lemos <60695991+PauloGLemos@users.noreply.github.com>
2022-10-07 22:37:06 -0300

    Page Product_Details
    Created Page Product Details

---
+++ cloud-walk-client/src/pages/product-details/style.ts
@@ -0,0 +1,80 @@
+import styled, { css } from "styled-components";
+import theme from "../../assets/styles/theme";
+
+export const Product = styled.div`
+  ${() => css`
+    background-color: ${theme.colors.primaryColor};
+    color: ${theme.colors.textColor};
+    display: flex;
+    align-itens: center;
+    justify-content:center;
+    flex-direction: column;
+    justify-content: space-between;
+  `}
+`;
+
+export const center = styled.div`
+  ${() => css`
+    background-color: ${theme.colors.primaryColor};
+    color: ${theme.colors.textColor};
+    display: flex;
+    align-itens: center;
+    justify-content:center;
+    flex-direction: row;
+    margin-top: 2rem;
+    justify-content: center;
+    height: 100vh;
+  `}
+`;
+
+export const Left = styled.div`
+  ${() => css`
+    background-color: #20232B;
+    color: ${theme.colors.textColor};
+    display: flex;
+    align-itens: center;
+    justify-content:center;
+    padding: 2rem;
+    border-radius: 1rem;
+    height: 55%;
+  `}
+`;
+
+export const Right = styled.div`
+  ${() => css`
+    background-color: #20232B;
+    color: ${theme.colors.textColor};
+    display: flex;
+    align-itens: center;
+    justify-content:flex-start;
+    flex-direction: column;
+    padding: 2rem;
+    margin: 1rem;
+    height: 50%;
+    width: 30%;
+  `}
+`;
+export const BtnAlign = styled.button`
+  ${() => css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: row;
+      background-color: #20232B;
+      width: 100%;
+  `}
+`;
+
+export const Btn = styled.button`
+  ${() => css`
+    background-color: #FF7337;
+    color: ${theme.colors.textColor};
+    font-height: bolder;
+    padding: 1rem;
+    margin: 1rem;
+    width: 100%;
+    :hover{
+      background-color: #EE4D2D;
+    }
+  `}
+`;
--- cloud-walk-client/src/router.tsx
+++ cloud-walk-client/src/router.tsx
@@ -1,12 +1,14 @@
 import Home from "./pages/home";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
+import Product from "./pages/product-details";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/product-detail" element={<Product/>} />
     </Routes>
   );
 };
